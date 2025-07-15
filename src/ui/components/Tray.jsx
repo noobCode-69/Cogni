074d19adb3d56d820d2a9d60232d9238b98cb438
noobCode-69 <armanyppph@gmail.com>
2025-07-15 16:53:55 +0530

    add font

--- src/ui/components/Tray.jsx
+++ src/ui/components/Tray.jsx
@@ -1,9 +1,12 @@
 const Tray = () => {
   return (
-    <div style={{
-      border : "1px solid #4a4a4a"
-    }} className="h-[100%] w-[100%] bg-black/40 text-white  flex items-center justify-center rounded-lg ">
-      Hello world
+    <div
+      style={{
+        border: "1px solid #4a4a4a",
+      }}
+      className="h-[100%] w-[100%] bg-black/40 flex items-center justify-center rounded-lg "
+    >
+      Ask
     </div>
   );
 };
