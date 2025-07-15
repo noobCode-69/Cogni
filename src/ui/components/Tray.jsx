27951572eba162016dc764bf5ad774b381e9ecff
noobCode-69 <armanyppph@gmail.com>
2025-07-15 17:05:15 +0530

    change border

--- src/ui/components/Tray.jsx
+++ src/ui/components/Tray.jsx
@@ -2,7 +2,7 @@ const Tray = () => {
   return (
     <div
       style={{
-        border: "1px solid #4a4a4a",
+        border: "0.9px solid #4a4a4a",
       }}
       className="h-[100%] w-[100%] bg-black/40 flex items-center justify-center rounded-lg "
     >
