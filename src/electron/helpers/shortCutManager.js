2f3b49041c1f6bb6ef51ff31faf9f1be267a2d2b
noobCode-69 <armanyppph@gmail.com>
2025-07-20 02:38:38 +0530

    add feature to register and unregister dynamic events

--- src/electron/helpers/shortCutManager.js
+++ src/electron/helpers/shortCutManager.js
@@ -5,6 +5,7 @@ export class ShortcutManager {
   constructor(mainWindowManager, appInstance) {
     this.mainWindowManager = mainWindowManager;
     this.app = appInstance;
+    this.dynamicShortcuts = new Map();
   }
 
   registerShortcuts() {
@@ -17,8 +18,15 @@ export class ShortcutManager {
     };
 
     keyboardShortcuts.forEach(
-      ({ accelerator, action, sendToRenderer, handler }) => {
-        const success = globalShortcut.register(accelerator, () => {
+      ({ accelerator, action, sendToRenderer, handler, dynamic }) => {
+        if (dynamic) {
+          this.dynamicShortcuts.set(accelerator, {
+            action,
+            sendToRenderer,
+          });
+          return;
+        }
+        globalShortcut.register(accelerator, () => {
           if (sendToRenderer) {
             window.webContents.send(
               EVENT_CONSTANTS.SEND_KEYBOARD_SHORTCUT_TO_RENDERER,
@@ -28,7 +36,40 @@ export class ShortcutManager {
             handler(context);
           }
         });
+
+        window.on("show", () => this.registerDynamicShortcuts());
+        window.on("hide", () => this.unregisterDynamicShortcuts());
+        window.on("close", () => this.unregisterDynamicShortcuts());
       }
     );
+  }
+
+  registerDynamicShortcuts() {
+    const window = this.mainWindowManager.getWindow();
+    if (!window) return;
+
+    for (const [
+      accelerator,
+      { action, sendToRenderer },
+    ] of this.dynamicShortcuts.entries()) {
+      if (!globalShortcut.isRegistered(accelerator)) {
+        globalShortcut.register(accelerator, () => {
+          if (sendToRenderer) {
+            window.webContents.send(
+              EVENT_CONSTANTS.SEND_KEYBOARD_SHORTCUT_TO_RENDERER,
+              action
+            );
+          }
+        });
+      }
+    }
+  }
+
+  unregisterDynamicShortcuts() {
+    for (const accelerator of this.dynamicShortcuts.keys()) {
+      if (globalShortcut.isRegistered(accelerator)) {
+        globalShortcut.unregister(accelerator);
+      }
+    }
   }
 }
